46ee9e8a920af3ea0e02a5e07968945cf03d7cbf
Jackson Shiffert <jshiffert@oki.org>
2023-06-12 23:01:43 -0400

    added current weather

--- assets/script.js
+++ assets/script.js
@@ -32,6 +32,35 @@ function initHistory() {
     renderSearchHistory();
 };
 
+function runWeather(data) {
+    var cityName = data.city.name;
+    var current = data.list[0];
+    var unix = current.dt * 1000;
+    var dateObj = new Date(unix).toDateString();
+    $('#city-title').text(cityName+', '+dateObj);
+    var currTemp = current.main.temp;
+    var currHumi = current.main.humidity;
+    var currWind = current.wind.speed;
+    var currIcon = current.weather[0].icon;
+    $('#today-temp').text('Temp: '+currTemp+'°F');
+    $('#today-wind').text('Wind: '+currWind+'mph');
+    $('#today-humidity').text('Humidity: '+currHumi+'%');
+    var iconUrl = "http://openweathermap.org/img/w/" + currIcon + ".png";
+    $('#wicon').attr('src', iconUrl);
+};
+
+function getApi(searchInput) {
+
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+searchInput+'&units=imperial&appid=441acd7fa99ced9a050d8990bc2b7139')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        runWeather(data);
+    });
+};
+
 $('#search-btn').click(function(){
     var searchInput = $('#search-input').val();
     if (searchInput === "") {
@@ -43,9 +72,9 @@ $('#search-btn').click(function(){
 
     storeHistory();
     renderSearchHistory();
+    getApi(searchInput);
 })
 
 initHistory();
-
 
 
